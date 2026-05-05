08e194964c8ea5a10f7d95d34b384fc85c404a6a
Ayyappan-Subramanian <ayyappansubramanian77@gmail.com>
2026-05-04 22:52:06 -0400

    Initial commit

---
+++ src/main/java/com/ayyappan/project/payshield/PayshieldApplication.java
@@ -0,0 +1,16 @@
+package com.ayyappan.project.payshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayshieldApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PayshieldApplication.class, args);
+
+		System.out.println("Iam Working");
+	}
+
+}
---
+++ src/test/java/com/ayyappan/project/payshield/PayshieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayyappan.project.payshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayshieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
